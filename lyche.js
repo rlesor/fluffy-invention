649677cbfec2b0c5c01cfdce473d7329608319c4
rlesor <erle.sorlie@gmail.com>
2016-11-07 11:05:22 +0100

    Update lyche.js

--- lyche.js
+++ lyche.js
@@ -1,3 +1,16 @@
-function validateForm() {
+function validateForm()
+{
+var pers=document.forms["reservasjon"]["res_antPers"].value;
+var tidspunkt = document.forms["reservasjon"]["res_tidspunkt"].value;
+var navn=document.forms["reservasjon"]["res_navn"].value;
+var tidspunkt=document.forms["reservasjon"]["res_tidspunkt"].value;
+var varighet=document.forms["reservasjon"]["res_varighet_min"].value;
+var telefonnummer=document.forms["reservasjon"]["res_telefonnummer"].value;
+var epost=document.forms["reservasjon"]["res_epost"].value;
 
+if (pers==null || pers=="" || pers>=12 || tidspunkt = "" || varighet == "Varighet" || navn==null || navn=="" || telefonnummer == null|| telefonnummer=="" || epost ==null || epost=="" ) 
+  {
+  alert("Please Fill All Required Field");
+  return false;
+  }
 }
