e7fe17264d7820278d7d13340969c93779863184
lenatorresdal <lena-torresdal@hotmail.com>
2016-11-09 12:46:26 +0100

    Update JavaScript_siste_versjon.js

--- JavaScript_siste_versjon.js
+++ JavaScript_siste_versjon.js
@@ -1,24 +1,22 @@
 //Gallery
 
-//This segment will create a gallery of images, where one can navigate the images using both buttons on a larger main image,
-//aswell as the mouse cursor, on a gallery underneath.
 
-let slideIndex = 1;              //The slideIndex decides which image that is shown as the main image
-function changeIndex(n, n2){	 //the parameter n is +1 (clicking right button) or -1 (left button) n2 is the slideIndex. See HTML.
-	slideIndex += n;
-	if(slideIndex > 9){    	 //If slideIndex
-		slideIndex = 1 ;
+let slideIndex_gallery = 1;
+function changeIndex(n, n2){
+	slideIndex_gallery += n;
+	if(slideIndex_gallery > 9){
+	slideIndex_gallery = 1 ;
 	}
-	if(slideIndex < 1){
-		slideIndex = 9;
+	if(slideIndex_gallery < 1){
+		slideIndex_gallery = 9;
 	}
-		
+
 	if(n2 != 0){
-		slideIndex = n2;
+		slideIndex_gallery = n2;
 	}
-		
-	changeClick(slideIndex)
-	console.log(slideIndex)
+
+	changeClick(slideIndex_gallery)
+	console.log(slideIndex_gallery)
 }
 
 
@@ -27,7 +25,7 @@ function changeClick(n){
 	let targetSrc = document.getElementById(bildeId);
 	document.getElementById("mainImg").src = targetSrc.getAttribute("src")
 }
-//Navigation bar 
+//Navigation bar
 
 //this javascript places the navigation bar at the right place when the user is navigating the page
 var y=0;         //variable that shows the where on the page the user is
@@ -39,18 +37,18 @@ window.onload = function img_size() {                                    //this 
 }
 function resize() {                                                      //this function puts the nav bar at the top of the div bar if the user resizes the window
   div = document.getElementById("nav_bar_div").getBoundingClientRect();
-  top_side = div.top; 
+  top_side = div.top;
 }
 window.onscroll = function nav_scroll() {                                //this function places the navbar under the logo if it's in frame, and at the top of the screen if the user scrolls down
   y = window.scrollY;                                                    //finds how many pixels down the user has scrolled
   top_side = div.top;                                                    //update top_side
-  if (top_side<0){                                                       //bugfix: if the user reloads the page while scrolled down on the page top_side gets a negative value. this resets the navbar position. 
+  if (top_side<0){                                                       //bugfix: if the user reloads the page while scrolled down on the page top_side gets a negative value. this resets the navbar position.
     window.scrollTo(0,0);
     div = document.getElementById("nav_bar_div").getBoundingClientRect();
-    top_side = div.top;           
+    top_side = div.top;
   }
   if (y>top_side){                                                       //checs if the user has scrolled past the div box where the nav bar is
-    document.getElementById("navigation_bar").style.position = "fixed";  //places the nav bar at the top  
+    document.getElementById("navigation_bar").style.position = "fixed";  //places the nav bar at the top
   }
   else{
     document.getElementById("navigation_bar").style.position = "absolute"; //places the nav bar under the logo/at the top of the wrapper.
@@ -59,8 +57,8 @@ window.onscroll = function nav_scroll() {                                //this 
 
 //endringer:
 //endre position absolute fra fixed til absolute i css (#navigation_bar)
-//laget en ny div med id tag, og laget css til den 
-//css må skrives slik at div skalerer med navbar 
+//laget en ny div med id tag, og laget css til den
+//css må skrives slik at div skalerer med navbar
 
 //Menu - show more information:
 
@@ -104,10 +102,13 @@ var tidspunkt=document.forms["reservasjon"]["res_tidspunkt"].value;
 var varighet=document.forms["reservasjon"]["res_varighet_min"].value;
 var telefonnummer=document.forms["reservasjon"]["res_telefonnummer"].value;
 var epost=document.forms["reservasjon"]["res_epost"].value;
+}
 
-if (pers==null || pers=="" || pers>=12 || tidspunkt = "" || varighet == "Varighet" || navn==null || navn=="" || telefonnummer == null|| telefonnummer=="" || epost ==null || epost=="" ) 
+/*
+if (pers==null || pers=="" || pers>=12 || tidspunkt = "" || varighet == "Varighet" || navn==null || navn=="" || telefonnummer == null|| telefonnummer=="" || epost ==null || epost=="" )
   {
   alert("Please Fill All Required Field");
   return false;
   }
 }
+*/
