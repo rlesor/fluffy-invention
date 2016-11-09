a8d36d2dd3fca2c844cfc66e4c526dac13133f1a
odamaa <odamaa@stud.ntnu.no>
2016-11-09 10:49:01 +0100

    Update lyche.js

--- lyche.js
+++ lyche.js
@@ -33,3 +33,25 @@ function scrollToTop() {
 	else clearTimeout(timeOut);
 }
 /* function found here: https://gist.github.com/ricardozea/abb9f98a19f6d04a0269 */
+
+/* slideshow */
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+/*slideshow ferdig*/
