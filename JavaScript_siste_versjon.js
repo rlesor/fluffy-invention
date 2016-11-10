943c8f790bf634adf6a97d9804fb53fe40dfa626
odamaa <odamaa@stud.ntnu.no>
2016-11-10 13:47:56 +0100

    Update JavaScript_siste_versjon.js

--- JavaScript_siste_versjon.js
+++ JavaScript_siste_versjon.js
@@ -73,25 +73,26 @@ function display_hide(id){
     }
 }
 
-// Slideshow:
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
+// Slideshow:
+
+let slideIndexSlideshow = 0;
+Slidesshow();
+
+function Slidesshow() {
+    let i;
+    const pictures = document.getElementsByClassName("slides");
+    const dots = document.getElementsByClassName("dot");
+    for (i = 0; i < pictures.length; i++) {
+       pictures[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1}
+    slideIndexSlideshow++;
+    if (slideIndexSlideshow > pictures.length) {slideIndexSlideshow = 1}
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    pictures[slideIndexSlideshow-1].style.display = "block";
+    dots[slideIndexSlideshow-1].className += " active";
+    setTimeout(Slidesshow, 4000); // Change image every 4 seconds
 }
 // Inspired by this tutorial: http://www.w3schools.com/howto/howto_js_slideshow.asp
-
